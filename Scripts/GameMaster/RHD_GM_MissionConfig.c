18556c5d7143b58b769037a136118e46557dff1f
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-05 10:38:23 -0400

    Add configurable reinforcement deployment delay

--- Scripts/GameMaster/RHD_GM_MissionConfig.c
+++ Scripts/GameMaster/RHD_GM_MissionConfig.c
@@ -5,6 +5,7 @@ class RHD_GM_MissionConfig
     int EnemyGroups = 2;
     int EnemyUnitsPerGroup = 4;
     int ReinforcementGroups = 1;
+    float ReinforcementDelaySeconds = 300.0;
     float MissionTimeoutSeconds = 1800.0;
     float SpawnDistance = 500.0;
     float CleanupDistance = 1500.0;
@@ -19,6 +20,7 @@ class RHD_GM_MissionConfig
         if (EnemyUnitsPerGroup > 20) EnemyUnitsPerGroup = 20;
         if (ReinforcementGroups < 0) ReinforcementGroups = 0;
         if (ReinforcementGroups > 20) ReinforcementGroups = 20;
+        if (ReinforcementDelaySeconds < 30.0) ReinforcementDelaySeconds = 30.0;
         if (MissionTimeoutSeconds < 60.0) MissionTimeoutSeconds = 60.0;
         if (SpawnDistance < 100.0) SpawnDistance = 100.0;
         if (CleanupDistance < SpawnDistance) CleanupDistance = SpawnDistance;
