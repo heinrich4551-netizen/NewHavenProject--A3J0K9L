60237438f10b610206d5ac11f47123dd6ab16774
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-05 10:36:21 -0400

    Wire unified mission activation into Game Master component

--- Scripts/GameMaster/RHD_GM_MissionGameMasterComponent.c
+++ Scripts/GameMaster/RHD_GM_MissionGameMasterComponent.c
@@ -5,12 +5,17 @@ class RHD_GM_MissionGameMasterComponentClass : ScriptComponentClass
 class RHD_GM_MissionGameMasterComponent : ScriptComponent
 {
     protected RHD_GM_MissionController m_Controller;
+    protected ref RHD_GM_MissionActivation m_Activation;
+    protected bool m_Bootstrapped;
 
     override void OnPostInit(IEntity owner)
     {
         super.OnPostInit(owner);
 
         m_Controller = new RHD_GM_MissionController();
+        m_Activation = new RHD_GM_MissionActivation(m_Controller);
+        RHD_GM_MissionBootstrap.RegisterDefaultMissions(m_Activation);
+        m_Bootstrapped = true;
     }
 
     RHD_GM_MissionController GetController()
@@ -18,9 +23,57 @@ class RHD_GM_MissionGameMasterComponent : ScriptComponent
         return m_Controller;
     }
 
-    // Scenario/Game Master integration should call this from its authoritative
-    // update path. Keeping the controller independent of frame/update APIs
-    // makes it reusable across scenarios and mission compositions.
+    RHD_GM_MissionActivation GetActivation()
+    {
+        return m_Activation;
+    }
+
+    RHD_GM_MissionEventDispatcher GetEventDispatcher()
+    {
+        if (!m_Activation)
+            return null;
+
+        return m_Activation.GetEvents();
+    }
+
+    bool IsBootstrapped()
+    {
+        return m_Bootstrapped;
+    }
+
+    bool RegisterMission(RHD_GM_MissionComposition composition)
+    {
+        if (!m_Activation)
+            return false;
+
+        return m_Activation.RegisterMission(composition);
+    }
+
+    RHD_GM_MissionInstance StartMissionById(string missionId)
+    {
+        if (!m_Activation)
+            return null;
+
+        return m_Activation.StartMission(missionId);
+    }
+
+    bool StopMission(RHD_GM_MissionInstance instance, bool failed = false)
+    {
+        if (!m_Activation)
+            return false;
+
+        return m_Activation.StopMission(instance, failed);
+    }
+
+    array<string> GetAvailableMissionIds()
+    {
+        if (!m_Activation || !m_Activation.GetLibrary())
+            return new array<string>();
+
+        return m_Activation.GetLibrary().GetMissionIds();
+    }
+
+    // Call from the authoritative scenario/Game Master update path.
     void TickMissions(float deltaSeconds)
     {
         if (m_Controller)
